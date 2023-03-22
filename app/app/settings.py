4348c9262b0d03944b6b69d14ace847f2c86ca7e
Islam <vvsalwayscodin@gmail.com>
2023-03-22 18:18:14 +0600

    removing a secret-key xd

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3x(jb9_x_wyy^7ir*lg1*-$$@n&wa)xm-59j!$-f3#su7p%_b1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
